b4ee74592e0da1f8e1643501a1f015c52f3ea28e
Laboratorio Computación N° 7 <claseslab7@inacap.cl>
2025-12-16 10:33:24 -0300

    avance clase 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ export default function Footer() {
     return (
         <footer className="footer">
             <p>© 2025 SportClub. Todos los derechos reservados.</p>
+            <p>By Nombre Apellido</p>
         </footer>
     );
 }
