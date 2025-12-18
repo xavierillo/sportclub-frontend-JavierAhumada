080e3aacac8ca5a3aa7c2d66d78914a890c9ba77
Laboratorio Computación N° 1 <claseslab1@inacap.cl>
2025-12-17 22:09:19 -0300

    clase 3 actividad

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "./Landing.css";
+import img_crossfit from "../assets/img/img_que_es_crossfit.png"
 
 export default function Landing() {
     return (
@@ -14,7 +15,15 @@ export default function Landing() {
                 <section className="clases">
                     <h3>Clases disponibles</h3>
                     <div className="cards">
-                        <div className="card">Crossfit</div>
+                        <div className="card">
+                            <img src={img_crossfit} alt="img crossfit" className="img_card" />
+                            <h3>Crossfit</h3>
+                            <p>es un programa de entrenamiento de alta intensidad
+                                que combina ejercicios variados 
+                                de diferentes disciplinas como 
+                                la halterofilia, gimnasia y entrenamiento 
+                                metabólico</p>
+                        </div>
                         <div className="card">Funcional</div>
                         <div className="card">Yoga</div>
                         <div className="card">Cardio</div>
